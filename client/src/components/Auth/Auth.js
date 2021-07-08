1e2e684cfc3807339269f9a69798886791c840e7
tinbui1908 <bhtt190800@gmail.com>
2021-07-08 22:34:33 +0700

    update Auth

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Auth = () => {
+    const state = null;
+    return (
+        <div>
+            AUTH
+        </div>
+    )
+}
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Typography} from '@material-ui/core';
+import {AppBar, Button, Typography, Toolbar, Avatar} from '@material-ui/core';
 import {Link} from 'react-router-dom';
 import useStyles from './styles';
 import memories from '../../images/memories.png';
@@ -14,17 +14,17 @@ const Navbar =()=>{
                 <Typography component={Link} className={classes.heading} variant="h2" align="center">Memories</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60"/>
             </div>
-            {/* <Toolbar className={classes.toolbar}>
+            <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className ={classes.profile}>
-                        <Avatar>
-                                
-                        </Avatar>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
                     </div>
                 ):(
-
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign in</Button>
                 )} 
-            </Toolbar> */}
+            </Toolbar>
         </AppBar>
     )
 };
