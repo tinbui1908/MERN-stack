2e944fb7e755ecab9a19b83c70173e86ee64d117
tinbui1908 <bhtt190800@gmail.com>
2021-07-08 20:28:53 +0700

    upcode

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+dotenv.config();
+const app = express();
+
+
+
+app.use(bodyParser.json({limit: "30mb",extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb",extended: true}));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+
+
+
+//connect mongoDB
+//const CONNECTION_URL = 'mongodb+srv://demomern:123@cluster0.ks3og.mongodb.net/MERN';
+const PORT = process.env.PORT || 5000;
+
+//console.log('RUNNING ON PORT ${PORT}');
+
+try {
+    mongoose.connect(process.env.CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
+    app.listen(PORT, () => console.log('Server is running on PORT: '+ PORT));
+} catch (error) {
+    console.error(error);
+};
+
+
+
