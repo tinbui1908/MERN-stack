7dc51b4bf804f1fd3bc63808a1ed573280f04fe5
tinbui1908 <bhtt190800@gmail.com>
2021-07-13 21:44:59 +0700

    fix error part 4

--- server/index.js
+++ server/index.js
@@ -2,24 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
-import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+import postRouter from './routes/posts.js';
 import userRouter from './routes/user.js';
 
 const app = express();
+dotenv.config();
 
-app.use(express.json({ limit: '30mb', extended: true }));
-app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use('/user', userRouter);
+app.use('/posts', postRouter);
+app.use('/users', userRouter);
 
+app.get('/', (req, res) => {
+    res.send('APP is running');
+});
+
+// mongodb
+//const CONNECTION_URL = 'mongodb+srv://MERNProject:MERNProject123@cluster0.b4hf8.mongodb.net/MERNProject';
 const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+    .then(() => app.listen(PORT, () => console.log('SERVER IS RUNNING ON PORT: ' + PORT)))
+    .catch((error) => console.log(error));
 
 mongoose.set('useFindAndModify', false);
